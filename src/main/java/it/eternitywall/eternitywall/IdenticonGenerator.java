7872d579b9a765c0e6d41925005c8974efb5c3ba
Riccardo Casatta <riccardo.casatta@gmail.com>
2015-11-24 12:04:02 +0100

    Identicon adapted to android Bitmap

--- src/main/java/it/eternitywall/eternitywall/IdenticonGenerator.java
+++ src/main/java/it/eternitywall/eternitywall/IdenticonGenerator.java
@@ -1,87 +1,60 @@
 package it.eternitywall.eternitywall;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.util.Log;
+
+import org.bitcoinj.core.Sha256Hash;
+
 public class IdenticonGenerator {
-	public static int height = 5;
-	public static int width = 5;
-	public static int mul = 13;
+    private static final String TAG = "IdenticonGenerator";
 
+    public static int SIDE = 7;
+    public static int MUL = 9;
 
-	/*
-	* Convert to Bitmap instead of BufferedImage or
-	* temporary use web service http://identicon-1132.appspot.com/data
-	* in any case this algo must produce the same identicon for the same alias address
-	* */
+    public static Bitmap generate(String string, int side, int mul) {
+        return generate(Sha256Hash.create(string.getBytes()).getBytes(), side, mul);
+    }
 
-	/*
-	public static BufferedImage generate(String string) {
-		return generate(Sha256Hash.create(string.getBytes()).getBytes());
+	public static Bitmap generate(String string) {
+		return generate(Sha256Hash.create(string.getBytes()).getBytes(), SIDE, MUL);
 	}
 
-
-
-	public static BufferedImage generate(byte[] hash) {
-
-
-		BufferedImage identicon = new BufferedImage(width*mul, height*mul, BufferedImage.TYPE_INT_ARGB);
-		WritableRaster raster = identicon.getRaster();
+	public static Bitmap generate(byte[] hash, int side, int mul) {
+		final Bitmap identicon = Bitmap.createBitmap(side*mul, side*mul, Bitmap.Config.ARGB_8888);
 
 		//get byte values as unsigned ints
 		int r = hash[0] & 255;
 		int g = hash[1] & 255;
 		int b = hash[2] & 255;
-		System.out.println("r " + r + " g " + g + " b " + b);
+		Log.i(TAG, "r " + r + " g " + g + " b " + b);
 
-		int [] background = new int [] {255,255,255, 0};
-		int [] foreground = new int [] {r, g, b, 255};
+		int background = Color.argb(0,255,255,255);
+		int foreground = Color.argb(255,r,g,b);
 
-		for(int x=0 ; x < width ; x++) {
-
-			int i = x < 3 ? x : width - 1 - x;
-			for(int y=0 ; y < height; y++) {
-				int [] pixelColor;
-
+		for(int x=0 ; x < side ; x++) {
+			int i = x < side/2 ? x : side - 1 - x;
+            final int i1 = x * mul;
+			for(int y=0 ; y < side; y++) {
+                final int i2 = y * mul;
+                int currentColor;
 
 				if((hash[i] >> y & 1) == 1)
-					pixelColor = foreground;
+                    currentColor=foreground;
 				else
-					pixelColor = background;
+                    currentColor=background;
 
-                System.out.println("i " + i + " y " + y + " " + (hash[i] >> y & 1) );
-
-                for(int a=0;a<mul;a++) {
-					for(int c=0;c<mul;c++) {
-						raster.setPixel(x * mul + a, y * mul + c, pixelColor);
-					}
-				}
-			}
+                for (int a = 0; a < mul; a++) {
+                    final int x1 = i1 + a;
+                    for (int c = 0; c < mul; c++) {
+                        final int y1 = i2 + c;
+                        identicon.setPixel(x1, y1, currentColor);
+                    }
+                }
+            }
 		}
 
 		return identicon;
 	}
-	*/
-	/**
-	 * Encode image to string
-	 * @param image The image to encode
-	 * @param type jpeg, bmp, ...
-	 * @return encoded string
-	 */
-	/*
-	public static String encodeToString(BufferedImage image, String type) {
-		String imageString = null;
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 
-		try {
-			ImageIO.write(image, type, bos);
-			byte[] imageBytes = bos.toByteArray();
-
-			BASE64Encoder encoder = new BASE64Encoder();
-			imageString = encoder.encode(imageBytes);
-
-			bos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return imageString;
-	}
-	*/
 }
