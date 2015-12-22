3f9d57cf4f465123d0eb6b8704697a0e69fdf819
Riccardo Casatta <riccardo.casatta@gmail.com>
2015-12-22 10:07:01 +0100

    EW Donation derivation

--- src/main/java/it/eternitywall/eternitywall/wallet/EWDerivation.java
+++ src/main/java/it/eternitywall/eternitywall/wallet/EWDerivation.java
@@ -4,6 +4,8 @@ import org.bitcoinj.crypto.ChildNumber;
 import org.bitcoinj.crypto.DeterministicKey;
 import org.bitcoinj.crypto.HDKeyDerivation;
 
+import it.eternitywall.eternitywall.bitcoin.Bitcoin;
+
 /**
  * Created by Riccardo Casatta @RCasatta on 17/11/15.
  */
@@ -27,6 +29,11 @@ public class EWDerivation {
     // coin_type http://doc.satoshilabs.com/slips/slip-0044.html#registered-coin-types
     // type could be 0 for changes and 1 for messages
 
+    //EW donation master public
+    private String hexMasterPublicKey="02982d773156ecf4809db9f25a8c827cffc2d1fff275b2691f87ad2ff608839fab";
+    private String hexChainCode="c662a4b9aea3f9cbc749e3789668451b1279be2424ab2539f2de64511198ad10";
+    private DeterministicKey donationMasterPublic;
+
     public EWDerivation(byte[] seed) {
         deterministicKey   = HDKeyDerivation.createMasterPrivateKey(seed);
         ewMaster           = HDKeyDerivation.deriveChildKey(deterministicKey, new ChildNumber(EW_DERIVATION, true));   // /m/4544288'/
@@ -42,6 +49,10 @@ public class EWDerivation {
         anonymousChangesMaster  = HDKeyDerivation.deriveChildKey(anonymousMaster, new ChildNumber(0,false) );  // /m/4544288'/1/0
         anonymousMessagesMaster = HDKeyDerivation.deriveChildKey(anonymousMaster, new ChildNumber(1,false) );  // /m/4544288'/1/1
         */
+
+        final byte[] bytesMasterPublicKey = Bitcoin.fromHex(hexMasterPublicKey);
+        final byte[] bytesChainCode       = Bitcoin.fromHex(hexChainCode);
+        donationMasterPublic  = HDKeyDerivation.createMasterPubKeyFromBytes(bytesMasterPublicKey, bytesChainCode);
     }
 
     public DeterministicKey getAlias() {
@@ -57,9 +68,14 @@ public class EWDerivation {
     }
 
     public DeterministicKey getAccount(int k) {
-        return HDKeyDerivation.deriveChildKey(bitcoinMaster, new ChildNumber(0, true));  // /m/4544288'/0'/0'
+        return HDKeyDerivation.deriveChildKey(bitcoinMaster, new ChildNumber(k, true));  // /m/4544288'/0'/0'/k
     }
 
 
 
+
+    public DeterministicKey getDonation(int d) {
+        return HDKeyDerivation.deriveChildKey(donationMasterPublic, new ChildNumber(d, false));
+
+    }
 }
--- src/test/java/it/eternitywall/BitcoinTest.java
+++ src/test/java/it/eternitywall/BitcoinTest.java
@@ -3,10 +3,12 @@ package it.eternitywall;
 import org.bitcoinj.crypto.ChildNumber;
 import org.bitcoinj.crypto.DeterministicKey;
 import org.bitcoinj.crypto.HDKeyDerivation;
+import org.bitcoinj.params.MainNetParams;
 import org.junit.Test;
 import org.spongycastle.util.encoders.Hex;
 
 import it.eternitywall.eternitywall.bitcoin.Bitcoin;
+import it.eternitywall.eternitywall.wallet.EWDerivation;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -15,28 +17,36 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class BitcoinTest {
 
-
+    /**
+     * hexMasterPublicKey: 02982d773156ecf4809db9f25a8c827cffc2d1fff275b2691f87ad2ff608839fab
+     * hexChainCode: c662a4b9aea3f9cbc749e3789668451b1279be2424ab2539f2de64511198ad10
+     */
 
     @Test
-    public void testing() {
-        final String passphrase = Bitcoin.getNewMnemonicPassphrase(Bitcoin.getLongRandomSeed());
+    public void testEWClientMasterpublic() {
+        byte[] entropyFromPassphrase = Bitcoin.getEntropyFromPassphrase("The passphrase is the Eternity Wall one");
+        EWDerivation ewDerivation = new EWDerivation(entropyFromPassphrase);
+        String s = ewDerivation.getAlias().toAddress(MainNetParams.get()).toString();
+        assertEquals("15uVaRjnY8atrWbe4v9nFimiR2JA3z1Sq8",s);
 
-        System.out.println("passphrase: " + passphrase);
+        DeterministicKey account = ewDerivation.getAccount(1);
+        String s1 = account.toAddress(MainNetParams.get()).toString();
+        assertEquals("1CUMBk95wj6K4BPvQLybJH3G61MDacMaEo",s1);
 
-        final DeterministicKey deterministicKey = Bitcoin.keyFromMnemonic(passphrase);
 
-        final DeterministicKey deriveSoft = HDKeyDerivation.deriveChildKey(deterministicKey, new ChildNumber(1, false));
+        final DeterministicKey deriveSoft = HDKeyDerivation.deriveChildKey(account, new ChildNumber(1, false));
         final String expected = Bitcoin.keyToStringAddress(deriveSoft);
-
 
         final DeterministicKey deriveSoft2 = HDKeyDerivation.deriveChildKey(deriveSoft, new ChildNumber(1, false));
         final String expected2 = Bitcoin.keyToStringAddress(deriveSoft2);
 
-
-        final String hexMasterPublicKey = Hex.toHexString(deterministicKey.getPubKey());
+        final String hexMasterPublicKey = Hex.toHexString(account.getPubKey());
         System.out.println("hexMasterPublicKey: " + hexMasterPublicKey);
-        final String hexChainCode       = Hex.toHexString(deterministicKey.getChainCode());
+        final String hexChainCode       = Hex.toHexString(account.getChainCode());
         System.out.println("hexChainCode: " + hexChainCode);
+
+        assertEquals("02982d773156ecf4809db9f25a8c827cffc2d1fff275b2691f87ad2ff608839fab", hexMasterPublicKey);
+        assertEquals("c662a4b9aea3f9cbc749e3789668451b1279be2424ab2539f2de64511198ad10",hexChainCode);
 
         final byte[] bytesMasterPublicKey = Bitcoin.fromHex(hexMasterPublicKey);
         final byte[] bytesChainCode       = Bitcoin.fromHex(hexChainCode);
