8da48537ef997b331a6786a322fd9dbe6b9ad294
lvaccaro <luck87@gmail.com>
2016-10-13 15:19:38 +0200

    New commit with gpg

--- src/main/java/it/eternitywall/eternitywall/fragments/NotarizeListFragment.java
+++ src/main/java/it/eternitywall/eternitywall/fragments/NotarizeListFragment.java
@@ -56,6 +56,8 @@ import static java.net.URLEncoder.encode;
  * Use the {@link NotarizeListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
+
 public class NotarizeListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
